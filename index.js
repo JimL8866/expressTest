78b9255196d29c392bc9df07d78d85b85b90a857
Jim <chengxuyuan9527@gmail.com>
2022-02-28 14:08:56 +1100

    update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import router from "./router.js"
 const app = express()
 // app.use(get)
 // app.use(post)
-app.use(router)
+app.use("/api", router)
 
 app.listen(80, ()=>console.log("server is currently running on http://127.0.0.1/"))
 
